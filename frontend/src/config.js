8122e149402359fbca7e34fa5e156996bd858d5b
IlluminoStudio <illumino.studio.324@gmail.com>
2025-09-18 15:46:25 +1000

    fix typo

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -26,8 +26,8 @@ export const getUrl = (path) => {
 
 // API Endpoints
 export const API_ENDPOINTS = {
-  TOPICS_SUGGEST: 'http://localhost:3001/api/v1/topics:suggest',
-  // TOPICS_SUGGEST: 'https://my.api.mockaroo.com/api/v1/topics:suggest',
+  // TOPICS_SUGGEST: 'http://localhost:3001/api/v1/topics:suggest',
+  TOPICS_SUGGEST: 'https://my.api.mockaroo.com/api/v1/topics:suggest',
   // POSTS_GENERATE: 'http://localhost:3001/api/v1/posts:generate',
-  POSTS_GENERATE: 'https://my.api.ockaroo.com/api/v1/posts:generate',
+  POSTS_GENERATE: 'https://my.api.mockaroo.com/api/v1/posts:generate',
 }; 
